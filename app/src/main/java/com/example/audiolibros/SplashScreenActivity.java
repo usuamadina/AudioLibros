3ba00940e73886df9fd77b70c512034592a0e0e0
Usua Madina <u.madina.i@gmail.com>
2017-01-05 18:31:58 +0100

    Modificaciones en SplashScreen + Animación de transición

--- app/src/main/java/com/example/audiolibros/SplashScreenActivity.java
+++ app/src/main/java/com/example/audiolibros/SplashScreenActivity.java
@@ -1,10 +1,19 @@
 package com.example.audiolibros;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,36 +23,53 @@ import java.util.TimerTask;
  */
 
 public class SplashScreenActivity extends Activity {
-    //Duración de Splash Screen
-    private static final long SPLASH_SCREEN_DURACION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Establecer orientación
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         // Ocultar barra de título
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+
         setContentView(R.layout.splash_screen);
 
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
 
-                // Lanzar MainActivity
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        TextView textView = (TextView) findViewById(R.id.tituloSplash);
+        Typeface fuente = Typeface.createFromAsset(getAssets(),"fonts/Bevan.ttf");
+        textView.setTypeface(fuente);
+
+        //Animación
+      //  Animation animationZoom = AnimationUtils.loadAnimation(this, R.anim.zoom);
+      //  imageView.startAnimation(animationZoom);
+        Animation animationAlpha = AnimationUtils.loadAnimation(this, R.anim.aparecer);
+        textView.startAnimation(animationAlpha);
+        animationAlpha.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
                 Intent mainIntent = new Intent().setClass(
                         SplashScreenActivity.this, MainActivity.class);
                 startActivity(mainIntent);
+                overridePendingTransition(R.anim.zoom,R.anim.menguar);
 
-
-                finish();
             }
-        };
 
-        Timer timer = new Timer();
-        timer.schedule(task, SPLASH_SCREEN_DURACION);
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
     }
+
 
 }
